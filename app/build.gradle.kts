93225e5d9d83878e2073a2dfb8481c2581b0ac87
ixsiid <ixsiid@halzion.net>
2023-10-22 18:06:52 +0900

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,24 +65,25 @@ android {
     //    }
 
     signingConfigs {
-        create("release") {
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-        }
+    // 署名付きビルドを一旦やめる
+    //  create("release") {
+    //      storeFile = file(keystoreProperties["storeFile"] as String)
+    //      storePassword = keystoreProperties["storePassword"] as String
+    //      keyAlias = keystoreProperties["keyAlias"] as String
+    //      keyPassword = keystoreProperties["keyPassword"] as String
+    //  }
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
-            isShrinkResources = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android.txt"),
-                "proguard-rules.pro",
-            )
-            signingConfig = signingConfigs.getByName("release")
-        }
+    //  release {
+    //      isMinifyEnabled = false
+    //      isShrinkResources = false
+    //      proguardFiles(
+    //          getDefaultProguardFile("proguard-android.txt"),
+    //          "proguard-rules.pro",
+    //      )
+    //      signingConfig = signingConfigs.getByName("release")
+    //  }
         debug {
         }
     }
